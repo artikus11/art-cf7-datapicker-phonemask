aa81dfc2b5f031364dbe062d3eccaee4bcccff3c
Artem Abramovich <artikus.sol@gmail.com>
2017-07-07 12:29:01 +0400

    first

---
+++ art-cf7-datapicker-phonemask.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Art Cf7 Datapicker Phonemask
+Plugin URI: #
+Description: Плагин телефонной маски и даты
+Version: 1.0
+Author: Артем Абрамович
+Author URI: №
+License:  GPL2
+*/
+
+/**
+ * Load jQuery script
+ */
+add_action( 'wp_enqueue_scripts', 'acdp_enqueue_datepicker' );
+function acdp_enqueue_datepicker() {
+	wp_enqueue_script( 'jquery-ui-datepicker' );
+	wp_register_style( 'jquery-ui', 'http://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css' );
+	wp_enqueue_style( 'jquery-ui' );
+	wp_enqueue_script('jquery-mask', plugins_url( '/assets/js/jquery.maskedinput.min.js', __FILE__ ), null, true);
+}
+
+add_action( 'wp_footer', 'acdp_custom_script' );
+function acdp_custom_script() {
+	?>
+	<script>
+        jQuery(document).ready(function ($) {
+            'use strict';
+            $.datepicker.setDefaults({
+                closeText: 'Закрыть',
+                prevText: '<Пред',
+                nextText: 'След>',
+                currentText: 'Сегодня',
+                monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+                monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+                dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+                dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+                dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+                weekHeader: 'Нед',
+                dateFormat: 'dd-mm-yy',
+                firstDay: 1,
+                showAnim: 'slideDown',
+                isRTL: false,
+                showMonthAfterYear: false,
+                yearSuffix: ''
+            } );
+            $('input[name*="date"], .datepicker, .acdp-datepicker').datepicker({
+	            dateFormat: 'dd.mm.yy'
+            });
+            
+            $(".acdp-phone").mask("(999) 999-9999");
+        });
+	</script>
+	
+	<?php
+}
